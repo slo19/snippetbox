27637b5224c572dea2a8703bc7cd02833bba2416
saulo-miranda <saulo.miranda@ufv.br>
2024-05-31 23:42:39 -0300

    using servemux

--- main.go
+++ main.go
@@ -9,9 +9,18 @@ func home(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("Hello from SnippetBox"))
 }
 
+func snippetView(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Display a specific SnippetBox..."))
+}
+
+func snippetCreate(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Create a new SnippetBox..."))
+}
 func main() {
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", home)
+	mux.HandleFunc("/snippet/view", snippetView)
+	mux.HandleFunc("/snippet/create", snippetCreate)
 
 	log.Print("Starting server on port 4000")
 	err := http.ListenAndServe(":4000", mux)
