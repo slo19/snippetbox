1dfce921a26c60059918ac75f84e0a5b4761e613
saulo-miranda <saulo.miranda@ufv.br>
2024-06-02 21:44:01 -0300

    http.error

--- main.go
+++ main.go
@@ -20,10 +20,8 @@ func snippetView(w http.ResponseWriter, r *http.Request) {
 func snippetCreate(w http.ResponseWriter, r *http.Request) {
 	if r.Method != "POST" {
 		w.Header().Set("Allow", "POST")
-		w.WriteHeader(405)
-		w.Write([]byte("Methid Not Allowed"))
+		http.Error(w, "Method Not Allowed", 405)
 		return
-
 	}
 	w.Write([]byte("Create a new SnippetBox..."))
 }
