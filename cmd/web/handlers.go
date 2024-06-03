3e006cf059a1b8b993d18209d23048c77055c797
saulo-miranda <saulo.miranda@ufv.br>
2024-06-03 20:57:36 -0300

    snippetcreate function

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -22,3 +22,12 @@ func snippetView(w http.ResponseWriter, r *http.Request) {
 	}
 	fmt.Fprintf(w, "Display a specific snippet with ID %d ...", id)
 }
+
+func snippetCreate(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPost {
+		w.Header().Set("Allow", http.MethodPost)
+		http.Error(w, "Method Not Allowed", http.StatusMethodNotAllowed)
+		return
+	}
+	w.Write([]byte("Create a new Snippet"))
+}
