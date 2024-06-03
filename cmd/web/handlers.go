3f93836241cf713eb28b9d9933d5e234770ee159
saulo-miranda <saulo.miranda@ufv.br>
2024-06-03 20:52:37 -0300

    snippetview function

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -13,3 +13,12 @@ func home(w http.ResponseWriter, r *http.Request) {
 	}
 	w.Write([]byte("Hello from SnippetBox"))
 }
+
+func snippetView(w http.ResponseWriter, r *http.Request) {
+	id, err := strconv.Atoi(r.URL.Query().Get("id"))
+	if err != nil || id < 1 {
+		http.NotFound(w, r)
+		return
+	}
+	fmt.Fprintf(w, "Display a specific snippet with ID %d ...", id)
+}
