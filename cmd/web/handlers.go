f443961392cf55d2d9bdd28f988d6689bff70230
saulo-miranda <saulo.miranda@ufv.br>
2024-06-05 22:54:10 -0300

    fix

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -15,7 +15,7 @@ func home(w http.ResponseWriter, r *http.Request) {
 	}
 	files := []string{
 		"./ui/html/pages/home.tmpl.html",
-		"./ui/html/pages/base.tmpl.html",
+		"./ui/html/base.tmpl.html",
 		"./ui/html/partials/nav.tmpl.html",
 	}
 	ts, err := template.ParseFiles(files...)
