aeb363dfe83383a00cae940a14ac653fc5ddee68
saulo-miranda <saulo.miranda@ufv.br>
2024-06-07 23:37:37 -0300

    longfile path in logging

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -13,7 +13,7 @@ func main() {
 	flag.Parse()
 
 	infoLog := log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
-	errorLog := log.New(os.Stderr, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile)
+	errorLog := log.New(os.Stderr, "ERROR\t", log.Ldate|log.Ltime|log.Llongfile)
 
 	mux := http.NewServeMux()
 	fileServer := http.FileServer(http.Dir("./ui/static/"))
