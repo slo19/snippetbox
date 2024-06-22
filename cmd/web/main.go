07d7a1905d1190aeb6c30d7a766bd4246a317675
slo19 <saulo.miranda19@gmail.com>
2024-06-22 20:41:21 -0300

    connection pool in main

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,10 +1,13 @@
 package main
 
 import (
+	"database/sql"
 	"flag"
 	"log"
 	"net/http"
 	"os"
+
+	"github.com/go-sql-driver/mysql"
 )
 
 type application struct {
@@ -15,10 +18,18 @@ type application struct {
 func main() {
 
 	addr := flag.String("addr", ":4000", "HTTP Network address")
+	dsn := flag.String("dsn", "web:pass@/snippetbox?parseTime=true", "MySQL data source name")
+
 	flag.Parse()
 
 	infoLog := log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
 	errorLog := log.New(os.Stderr, "ERROR\t", log.Ldate|log.Ltime|log.Llongfile)
+
+	db, err := openDB(*dsn)
+	if err != nil {
+		errorLog.Fatal(err)
+	}
+	defer db.Close()
 
 	app := &application{
 		errorLog: errorLog,
@@ -32,6 +43,6 @@ func main() {
 	}
 
 	infoLog.Printf("Starting a new server at %s", *addr)
-	err := srv.ListenAndServe()
+	err = srv.ListenAndServe()
 	errorLog.Fatal(err)
 }
