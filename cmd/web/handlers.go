c8186abd74670e27fa7a4507e33a49efc63fef59
slo19 <saulo.miranda19@gmail.com>
2024-07-16 17:23:15 -0300

    route fix

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -63,5 +63,5 @@ func (app *application) snippetCreatePost(w http.ResponseWriter, r *http.Request
 		return
 	}
 
-	http.Redirect(w, r, fmt.Sprintf("/snippet/view?id=%d", id), http.StatusSeeOther)
+	http.Redirect(w, r, fmt.Sprintf("/snippet/view/%d", id), http.StatusSeeOther)
 }
